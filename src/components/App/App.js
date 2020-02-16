677a7b7cac4e39b89e1c57c98dce31e79102b763
Kacper Witosław <wito77pl@gmail.com>
2020-02-16 15:02:36 +0100

    start react project

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className="app-content">
+          <h1>My first React app</h1>
+          <h2>Hello world!</h2>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default App;
