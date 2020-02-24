1d4b3f0b725a111e681617d904289826b9591983
Kacper Witosław <wito77pl@gmail.com>
2020-02-24 21:46:14 +0100

    Add changes to search reducer

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,16 +1,26 @@
 // selectors
 export const getSearchString = ({ searchString }) => searchString;
-export const countVisibleCards = ({ cards, searchString }) => cards.filter(card => new RegExp(searchString, 'i').test(card.title));
+
+export const countVisibleCards = ({ cards, searchString }) =>
+  cards.filter(card => new RegExp(searchString, 'i').test(card.title));
+
 export const countAllCards = ({ cards }) => cards.length;
+
 // action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createActionSearch = payload => ({ payload: { ...payload }, type: CHANGE });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
