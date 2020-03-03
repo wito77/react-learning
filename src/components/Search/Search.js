dc1f6e99800307315095ab7e8c0c25509ccdc67f
Kacper Witosław <wito77pl@gmail.com>
2020-03-03 18:33:28 +0100

    Fix for PropType

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -14,7 +14,7 @@ class Search extends React.Component {
     changeSearchString: PropTypes.func,
     countVisible: PropTypes.number,
     countAll: PropTypes.number,
-    history: PropTypes.node,
+    history: PropTypes.object,
   }
 
   static defaultProps = {
